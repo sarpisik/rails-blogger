47ba3725de0e315bbd5d6a85bc11ff20e754b134
sarpisik <contact@sarpisik.com>
2019-05-24 15:19:14 +0200

    Add flash notice for CREATE action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,8 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
