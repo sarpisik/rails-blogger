a5dba398098f2435a7bf530e5b1cb119b4a7b919
sarpisik <contact@sarpisik.com>
2019-05-24 14:38:31 +0200

    Edit create ACTION

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,4 +12,12 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+    @article.save
+    redirect_to article_path(@article)
+  end
 end
